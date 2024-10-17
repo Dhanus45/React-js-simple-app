330d769d323c375128cf990895ba1f89b11a8d04
Dhanush Shetty <133327430+Dhanus45@users.noreply.github.com>
2024-10-17 16:58:22 +0530

    Add files via upload

---
+++ src/components/Fruits.jsx
@@ -0,0 +1,4 @@
+export default function Fruit(){
+    const fruit=["Kiwi","Apple","grapes","mangos"]
+    return <div>{fruit}</div>;
+}
---
+++ src/components/Hello.jsx
@@ -0,0 +1,10 @@
+function Aer({person}){
+    return(<div>
+    <h1>Hello
+    {person.name}
+    {person.message}
+    </h1>
+    </div>
+    );
+}
+export default Aer;
